783a8d4798d61f6a4e3f1f2ad402c3fd5e87c7ff
Jeff Potter <jeff.potter6@gmail.com>
2015-02-04 14:36:53 -0700

    changing to yml, adding vc, and controller, and networking

--- generator.rb
+++ generator.rb
@@ -1,48 +1,73 @@
 require 'erb'
 require 'json'
+require 'yaml'
+require 'fileutils'
+
+TEMPLATES_DIR = "templates/"
+GENERATED_DIR = "gen/"
+
+# Cleaning
+
+FileUtils::rm_rf GENERATED_DIR
+puts 'Clean...'
+FileUtils::mkdir_p GENERATED_DIR
+FileUtils::mkdir_p GENERATED_DIR + 'models/'
+FileUtils::mkdir_p GENERATED_DIR + 'controllers/'
+FileUtils::mkdir_p GENERATED_DIR + 'network/'
+FileUtils::mkdir_p GENERATED_DIR + 'view_controllers/'
+
+# Load Templates
+
+model_template = File.read('templates/model.swift.erb')
+controller_template = File.read('templates/controller.swift.erb')
+request_provider_template = File.read('templates/request_provider.swift.erb')
+table_view_controller_template = File.read('templates/table_view_controller.swift.erb')
+
+# Load Config files
+resources = YAML.load(File.read('resources.yml'))
 
 # Models
-model_template = File.read('templates/model.swift.erb')
-models = JSON.parse(File.read('models.json'))
 
 puts 'Generating...'
 puts 'Models:'
 
-models.each do |model| 
-	@model = model
-	model_file = File.new('gen/models/' + @model['name'] + '.swift','w')
+resources.each do |resource| 
+	@model = resource
+	model_file = File.new('gen/models/' + @model['name'] + '.swift','w+')
 	model_file.write(ERB.new(model_template).result)
 	puts '- ' + @model['name'].capitalize
 end
 
 # Controllers
-controllers = JSON.parse(File.read('controllers.json'))
 
 puts 'Controllers:'
 
-controllers.each do |controller|
-	@ctrl = controller
-	controller_template = File.read('templates/controller.swift.erb')
-	controller_file = File.new('gen/controllers/' + @ctrl['name'].capitalize + 'Controller.swift', 'w')
+resources.each do |resource|
+	@ctrl = resource
+	controller_file = File.new('gen/controllers/' + @ctrl['name'].capitalize + 'Controller.swift', 'w+')
 	controller_file.write(ERB.new(controller_template).result)
 	puts '- ' + @ctrl['name'].capitalize + 'Controller'
 end
 
 puts 'Networking:'
 
-networking = JSON.parse(File.read('network.json'))
-
-networking.each do |network|
-	@net = network
-
-	request_provider_template = File.read('templates/request_provider.swift.erb')
-	request_provider_file = File.new('gen/network/' + @net['name'].capitalize + 'RequestProvider.swift','w')
+resources.each do |resource|
+	@net = resource
+	request_provider_file = File.new('gen/network/' + @net['name'].capitalize + 'RequestProvider.swift','w+')
 	request_provider_file.write(ERB.new(request_provider_template).result)
 	puts '- ' + @net['name'].capitalize + 'RequestProvider'
-
-	router_template = File.read('templates/router.swift.erb')
-	router_file = File.new('gen/network/' + @net['name'].capitalize + 'Router.swift', 'w')
-	router_file.write(ERB.new(router_template).result)
-	puts '- ' + @net['name'].capitalize + 'Router'
 	
 end
+
+puts 'ViewControllers:'
+
+resources.each do |resource|
+	@vc = resource
+	if @vc["methods"]["all"]
+		vc_file = File.new('gen/view_controllers/' + @vc['name'].capitalize + 'TableViewController.swift','w+')
+		vc_file.write(ERB.new(table_view_controller_template).result)
+		puts '- ' + @vc['name'].capitalize + 'TableViewController'
+	end
+end
+
+
