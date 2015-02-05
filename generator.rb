76b277a033230cdb1b4d28f07dae59e947a09bd2
Jeff Potter <jeff.potter6@gmail.com>
2015-02-04 17:43:21 -0700

    adding more networking

--- generator.rb
+++ generator.rb
@@ -18,56 +18,66 @@ FileUtils::mkdir_p GENERATED_DIR + 'view_controllers/'
 
 # Load Templates
 
-model_template = File.read('templates/model.swift.erb')
-controller_template = File.read('templates/controller.swift.erb')
-request_provider_template = File.read('templates/request_provider.swift.erb')
-table_view_controller_template = File.read('templates/table_view_controller.swift.erb')
+@model_template = File.read('templates/model.swift.erb')
+@controller_template = File.read('templates/controller.swift.erb')
+@request_provider_template = File.read('templates/request_provider.swift.erb')
+@table_view_controller_template = File.read('templates/table_view_controller.swift.erb')
 
 # Load Config files
 resources = YAML.load(File.read('resources.yml'))
 
-# Models
-
-puts 'Generating...'
-puts 'Models:'
-
-resources.each do |resource| 
+def generate_models(resource)
 	@model = resource
 	model_file = File.new('gen/models/' + @model['name'] + '.swift','w+')
-	model_file.write(ERB.new(model_template).result)
+	model_file.write(ERB.new(@model_template).result)
 	puts '- ' + @model['name'].capitalize
 end
 
-# Controllers
-
-puts 'Controllers:'
-
-resources.each do |resource|
+def generate_controllers(resource)
 	@ctrl = resource
 	controller_file = File.new('gen/controllers/' + @ctrl['name'].capitalize + 'Controller.swift', 'w+')
-	controller_file.write(ERB.new(controller_template).result)
+	controller_file.write(ERB.new(@controller_template).result)
 	puts '- ' + @ctrl['name'].capitalize + 'Controller'
 end
 
-puts 'Networking:'
-
-resources.each do |resource|
+def generate_networking(resource)
 	@net = resource
 	request_provider_file = File.new('gen/network/' + @net['name'].capitalize + 'RequestProvider.swift','w+')
-	request_provider_file.write(ERB.new(request_provider_template).result)
+	request_provider_file.write(ERB.new(@request_provider_template).result)
 	puts '- ' + @net['name'].capitalize + 'RequestProvider'
-	
 end
 
-puts 'ViewControllers:'
-
-resources.each do |resource|
+def generate_view_controllers(resource)
 	@vc = resource
 	if @vc["methods"]["all"]
 		vc_file = File.new('gen/view_controllers/' + @vc['name'].capitalize + 'TableViewController.swift','w+')
-		vc_file.write(ERB.new(table_view_controller_template).result)
+		vc_file.write(ERB.new(@table_view_controller_template).result)
 		puts '- ' + @vc['name'].capitalize + 'TableViewController'
 	end
+end
+
+puts 'Generating...'
+
+resources.each do |resource| 
+
+	puts 'Resource: ' + resource["name"]
+
+	if resource.has_key?("fields")
+		generate_models(resource)
+	end
+
+	if resource.has_key?("methods")
+		generate_controllers(resource)
+
+		if resource.has_key?("endpoint")
+			generate_networking(resource)
+		end
+	end
+
+	if resource.has_key?("views")
+		generate_view_controllers(resource)
+	end
+
 end
 
 
